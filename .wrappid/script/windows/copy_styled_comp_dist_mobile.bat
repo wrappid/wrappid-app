3aa92dd0309e15a7c358499cac7beb367d2a0957
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-04-29 23:38:59 +0530

    + wrappid sc-(web|mobile) script fix

--- .wrappid/script/windows/copy_styled_comp_dist_mobile.bat
+++ .wrappid/script/windows/copy_styled_comp_dist_mobile.bat
@@ -4,11 +4,11 @@ set PROJECT_DIR=%CD%
 echo Current working directory is
 echo %PROJECT_DIR%
 
-@REM build @wrappid/sc_web
-CD %WRAPPID_HOME%\sc_web\
+@REM build @wrappid/sc-mobile
+CD %WRAPPID_HOME%\sc-mobile\
 RD /S /Q dist
 call npm run build
 
-@REM Copying files from (%WRAPPID_HOME%\sc_web\dist\) directory to (.\.wrappid\temp\mobile\node_modules\@wrappid\styled_components\)...
-echo Copying files from (%WRAPPID_HOME%\sc_web\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled_components\)...
-XCOPY /e /v %WRAPPID_HOME%\sc_web\dist\ %PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled_components\
+@REM Copying files from (%WRAPPID_HOME%\sc-mobile\dist\) directory to (.\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
+echo Copying files from (%WRAPPID_HOME%\sc-mobile\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
+XCOPY /e /v %WRAPPID_HOME%\sc-mobile\dist\ %PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\
