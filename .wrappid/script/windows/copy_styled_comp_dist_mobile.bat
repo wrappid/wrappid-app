6e10f9b15951da6bd998ea1659497e07703a026c
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-05-02 08:04:15 +0530

    + script related changes for sc to native and added stageapi to config

--- .wrappid/script/windows/copy_styled_comp_dist_mobile.bat
+++ .wrappid/script/windows/copy_styled_comp_dist_mobile.bat
@@ -4,11 +4,11 @@ set PROJECT_DIR=%CD%
 echo Current working directory is
 echo %PROJECT_DIR%
 
-@REM build @wrappid/sc-mobile
-CD %WRAPPID_HOME%\sc-mobile\
+@REM build @wrappid/native-mobile
+CD %WRAPPID_HOME%\native-mobile\
 RD /S /Q dist
 call npm run build
 
-@REM Copying files from (%WRAPPID_HOME%\sc-mobile\dist\) directory to (.\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
-echo Copying files from (%WRAPPID_HOME%\sc-mobile\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
-XCOPY /e /v %WRAPPID_HOME%\sc-mobile\dist\ %PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\
+@REM Copying files from (%WRAPPID_HOME%\native-mobile\dist\) directory to (.\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
+echo Copying files from (%WRAPPID_HOME%\native-mobile\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\)...
+XCOPY /e /v %WRAPPID_HOME%\native-mobile\dist\ %PROJECT_DIR%\.wrappid\temp\mobile\node_modules\@wrappid\styled-components\
--- .wrappid/script/windows/copy_styled_comp_dist_web.bat
+++ .wrappid/script/windows/copy_styled_comp_dist_web.bat
@@ -4,11 +4,11 @@ set PROJECT_DIR=%CD%
 echo Current working directory is
 echo %PROJECT_DIR%
 
-@REM build @wrappid/sc-web
-CD %WRAPPID_HOME%\sc-web\
+@REM build @wrappid/native-web
+CD %WRAPPID_HOME%\native-web\
 RD /S /Q dist
 call npm run build
 
-@REM Copying files from (%WRAPPID_HOME%\sc-web\dist\) directory to (.\.wrappid\temp\web\node_modules\@wrappid\styled-components\)...
-echo Copying files from (%WRAPPID_HOME%\sc-web\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\web\node_modules\@wrappid\styled-components\)...
-XCOPY /e /v %WRAPPID_HOME%\sc-web\dist\ %PROJECT_DIR%\.wrappid\temp\web\node_modules\@wrappid\styled-components\
+@REM Copying files from (%WRAPPID_HOME%\native-web\dist\) directory to (.\.wrappid\temp\web\node_modules\@wrappid\styled-components\)...
+echo Copying files from (%WRAPPID_HOME%\native-web\dist\) directory to (%PROJECT_DIR%\.wrappid\temp\web\node_modules\@wrappid\styled-components\)...
+XCOPY /e /v %WRAPPID_HOME%\native-web\dist\ %PROJECT_DIR%\.wrappid\temp\web\node_modules\@wrappid\styled-components\
