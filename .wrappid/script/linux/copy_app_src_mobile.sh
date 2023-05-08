14bcccfa761946f9724c2ce28990e6348594ef40
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-05-08 18:14:29 +0530

    + clean

--- .wrappid/script/linux/copy_app_src_mobile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Copying files from (./src) directory to (./.wrappid/temp/mobile/src)..."
-cp -r ./src/ ./.wrappid/temp/mobile/src/
--- .wrappid/script/linux/copy_app_src_web.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Copying files from (./src) directory to (./.wrappid/temp/web/src)..."
-cp -r ./src/ ./.wrappid/temp/web/src/
