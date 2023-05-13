ef27f6461ee700b99cbbb6afc54681b14615c2e1
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-05-13 17:08:51 +0530

    + src module rename to modules

--- src/ComponetRegistry.js
+++ src/ComponetRegistry.js
@@ -1,21 +1,19 @@
 import { CoreH6, CoreIcon } from "@wrappid/core";
 
-import CheckUserExist from "./module/auth/CheckUserExist";
-import LoginWithOtp from "./module/auth/LoginWithOtp";
-import LoginWithPassword from "./module/auth/LoginWithPassword";
-import RegisterOrResetPassword from "./module/auth/RegisterOrResetPassword";
+import CheckUserExist from "./modules/auth/CheckUserExist";
+import LoginWithOtp from "./modules/auth/LoginWithOtp";
+import LoginWithPassword from "./modules/auth/LoginWithPassword";
+import RegisterOrResetPassword from "./modules/auth/RegisterOrResetPassword";
 
 export const ComponentRegistry = {
-  Dashboard: {
-    comp: (
-      <CoreH6>
-        Dashboard <CoreIcon type="rxi">alcohol</CoreIcon>
-      </CoreH6>
-    ),
-  },
-  checkUserExist: { comp: <CheckUserExist /> },
-  loginWithOtp: { comp: <LoginWithOtp /> },
-  loginWithPassword: { comp: <LoginWithPassword /> },
-  register: { comp: <RegisterOrResetPassword /> },
-  resetPassword: { comp: <RegisterOrResetPassword /> },
+    Dashboard: {
+        comp: (
+            <CoreH6>Dashboard <CoreIcon type="rxi">alcohol</CoreIcon></CoreH6>
+        ),
+    },
+    checkUserExist   : { comp: <CheckUserExist /> },
+    loginWithOtp     : { comp: <LoginWithOtp /> },
+    loginWithPassword: { comp: <LoginWithPassword /> },
+    register         : { comp: <RegisterOrResetPassword /> },
+    resetPassword    : { comp: <RegisterOrResetPassword /> },
 };
