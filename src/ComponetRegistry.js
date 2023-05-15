ad6bae4a7b1236ac124ac94a68c0c97129f4a497
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-05-15 19:01:52 +0530

    + add registry

--- src/ComponetRegistry.js
+++ src/ComponetRegistry.js
@@ -1,14 +1,12 @@
 import { CoreH6, CoreIcon } from "@wrappid/core";
 
-import Components from "./modules/dev/components/Components";
-import StyleUtilities from "./modules/dev/components/StyleUtilities";
+import modulesComponentsRegistry from "./modules/modules.components.registry";
 
 export const ComponentRegistry = {
-    Components: { comp: <Components /> },
-    Dashboard : {
+    Dashboard: {
         comp: (
             <CoreH6>Dashboard <CoreIcon type="rxi">alcohol</CoreIcon></CoreH6>
         ),
     },
-    StyleUtilities: { comp: <StyleUtilities /> },
+    ...modulesComponentsRegistry
 };
