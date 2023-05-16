d50b3e839d8531e083c3047b442569f38b6bd657
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-05-16 10:57:57 +0530

    + modules form component included

--- src/modules/app-builder/components.registry.js
+++ src/modules/app-builder/components.registry.js
@@ -1,8 +1,10 @@
 
 import Components from "./components/Components";
+import FormsManager from "./components/FormsManager";
 import StyleUtilities from "./components/StyleUtilities";
 
 export const ComponentRegistry = {
     Components    : { comp: <Components /> },
-    StyleUtilities: { comp: <StyleUtilities /> },
+    FormsManager  : { comp: <FormsManager /> },
+    StyleUtilities: { comp: <StyleUtilities /> }
 };
