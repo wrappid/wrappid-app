32999e81c77c8224630d1e2ab90692398455805e
Sambuddha Chatterjee <sam@rxefy.com>
2023-08-12 17:13:04 +0530

    Update ResourcesRegistry.js
    corrected the path of the image resources

--- src/registry/ResourcesRegistry.js
+++ src/registry/ResourcesRegistry.js
@@ -1,8 +1,8 @@
 import modulesResourcesRegistry from "../modules/modules.resources.registry";
 
 export const ResourcesRegistry = {
-    appLogo     : require("./images/logo.png"),
-    appLogoDark : require("./images/logo_dark.png"),
-    appLogoLight: require("./images/logo_light.png"),
+    appLogo     : require("../images/logo.png"),
+    appLogoDark : require("../images/logo_dark.png"),
+    appLogoLight: require("../images/logo_light.png"),
     ...modulesResourcesRegistry 
 };
