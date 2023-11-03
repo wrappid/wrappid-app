b1057927fcd47b05d886edd7f46da060606b3e14
anantakumarghosh <antaghosh@gmail.com>
2023-11-03 17:26:48 +0530

    + routes registry added

---
+++ src/registry/RoutesRegistry.js
@@ -0,0 +1,11 @@
+import modulesRoutesRegistry from "../modules/modules.routes.registry";
+
+export const RoutesRegistry = {
+    defaultAppRoute: {
+        Page: { appComponent: "AppComponent" }, //required in page container
+        authRequired: false,
+        entityRef: "defaultAppRoute", //required for routeregistry
+        url: "defaultAppRoute"
+    }, 
+    ...modulesRoutesRegistry
+};
