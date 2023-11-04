39e9bbc0bdfc2c0b4ffb8922f037a77d4a3d44c6
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-11-04 10:05:01 +0530

    + wrappid static page created

--- src/registry/ComponentsRegistry.js
+++ src/registry/ComponentsRegistry.js
@@ -1,3 +1,4 @@
+import StaticWebPage from "../components/StaticWebPage";
 import modulesComponentsRegistry from "../modules/modules.components.registry";
 
-export const ComponentsRegistry = { ...modulesComponentsRegistry };
+export const ComponentsRegistry = { ...modulesComponentsRegistry, AppComponent: StaticWebPage };
--- src/registry/RoutesRegistry.js
+++ src/registry/RoutesRegistry.js
@@ -2,10 +2,10 @@ import modulesRoutesRegistry from "../modules/modules.routes.registry";
 
 export const RoutesRegistry = {
     defaultAppRoute: {
-        Page: { appComponent: "AppComponent" }, //required in page container
+        Page        : { appComponent: "AppComponent" }, //required in page container
         authRequired: false,
-        entityRef: "defaultAppRoute", //required for routeregistry
-        url: "defaultAppRoute"
+        entityRef   : "defaultAppRoute", //required for routeregistry
+        url         : "defaultAppRoute"
     }, 
     ...modulesRoutesRegistry
 };
