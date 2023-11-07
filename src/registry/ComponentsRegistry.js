4a0ddf544b0fbc9406f645fa90a3d91fa0a9b3dc
anantakumarghosh <antaghosh@gmail.com>
2023-11-07 16:29:54 +0530

    + fix component registry structure

--- src/registry/ComponentsRegistry.js
+++ src/registry/ComponentsRegistry.js
@@ -1,4 +1,7 @@
 import StaticWebPage from "../components/StaticWebPage";
 import modulesComponentsRegistry from "../modules/modules.components.registry";
 
-export const ComponentsRegistry = { ...modulesComponentsRegistry, AppComponent: StaticWebPage };
+export const ComponentsRegistry = {
+    ...modulesComponentsRegistry,
+    AppComponent: { comp: StaticWebPage }
+};
