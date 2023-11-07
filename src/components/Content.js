2b42811e89127aa995813370958b56bee306bd99
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-11-07 16:52:12 +0530

    + statuc web page basic

--- src/components/Content.js
+++
@@ -1,9 +0,0 @@
-import { CoreTypographyBody1 } from "@wrappid/core";
-
-function Content() {
-    return (
-        <CoreTypographyBody1>Content</CoreTypographyBody1>
-    );
-}
-
-export default Content;
--- src/components/StaticWebPage.js
+++ src/components/StaticWebPage.js
@@ -1,13 +1,17 @@
-import Content from "./Content";
-import Header from "./Header";
+import { CoreClasses, CoreH4, CoreImage, CoreStack, CoreTypographyBody1 } from "@wrappid/core";
 
 function StaticWebPage() {
     return (
-        <>
-            <Header />
+        <CoreStack direction="column" styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
+            <CoreImage
+                src="https://avatars.githubusercontent.com/u/128048526?s=200&v=4"
+                alt="Wrappid Logo"
+            />
 
-            <Content />
-        </>
+            <CoreH4>Wrappid Framework</CoreH4>
+
+            <CoreTypographyBody1>Wrappid Framework is a wrapper framework built to enable rapid development of applications based on React, React Native, Node, Express etc.</CoreTypographyBody1>
+        </CoreStack>
     );
 }
 
