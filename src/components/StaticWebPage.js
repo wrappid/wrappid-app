a536303b5a208c287ec0748aa1951cb4f43ab746
anantakumarghosh <antaghosh@gmail.com>
2023-11-08 08:13:34 +0530

    + initial static homepage
    Ref: #9

--- src/components/StaticWebPage.js
+++ src/components/StaticWebPage.js
@@ -1,17 +1,15 @@
-import { CoreClasses, CoreH4, CoreImage, CoreStack, CoreTypographyBody1 } from "@wrappid/core";
+import { CoreClasses, CoreH4, CoreStack, CoreTypographyBody1 } from "@wrappid/core";
+import Welcome from "./Welcome";
 
 function StaticWebPage() {
     return (
+        <>
         <CoreStack direction="column" styleClasses={[CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER]}>
-            <CoreImage
-                src="https://avatars.githubusercontent.com/u/128048526?s=200&v=4"
-                alt="Wrappid Logo"
-            />
-
             <CoreH4>Wrappid Framework</CoreH4>
-
             <CoreTypographyBody1>Wrappid Framework is a wrapper framework built to enable rapid development of applications based on React, React Native, Node, Express etc.</CoreTypographyBody1>
         </CoreStack>
+        <Welcome />
+        </>
     );
 }
 
